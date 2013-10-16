a5b78a0e69d9b0fcd7c1048a7b43bbb28328ef0c
David Ochoa <ochoa@ebi.ac.uk>
2013-10-16 18:56:50 +0100

    lots of changes

--- js/draw_graph.js
+++ js/draw_graph.js
@@ -10,71 +10,73 @@
     // var NETWORK_DATA_URI = 'http://localhost:9988/v1/networks/views';
 //    var NETWORK_LOCAL_DATA_URI = 'data/galFiltered2.json';
     // var VISUAL_STYLE_URI = 'data/vs.json';
-    var NETWORK_LOCAL_DATA_URI = 'data/netJSON.d3';
+    var NETWORK_LOCAL_DATA_URI = 'data/netJSON.min.d3';
 //    var VISUAL_STYLE_URI = 'data/vs.json';
     var NETWORK_WINDOW_TAG = "#network-view";
 	
-	var n = 30;
+	var n = 8;
+	var r = 5;
+    var trans=[0,0]
+    var scale=1;
 	var width = $(NETWORK_WINDOW_TAG).width(),
 	    height = $(NETWORK_WINDOW_TAG).height();
 	var color = d3.scale.category20();	
+	var previousd;
 	
-	
-	var force = d3.layout.force()
-	    // .charge(-120)
-	    // .linkDistance(30)
-	    .size([width, height]);
-
 	var vis = d3.select(NETWORK_WINDOW_TAG)
 		.append("svg")
+        	.attr("id", "playgraph")
+	        .attr({
+	          "width": width,
+	          "height": height
+	        })
         	.attr("viewBox", "0 0 " + width + " " + height )
         	.attr("preserveAspectRatio", "xMidYMid meet")
 		    .attr("pointer-events", "all")
-		.append('svg:g')
-		    .call(d3.behavior.zoom().on("zoom", redraw))
-		.append('svg:g');
-		
-		function redraw() {
-		  console.log("here", d3.event.translate, d3.event.scale);
-		  vis.attr("transform",
-		      "translate(" + d3.event.translate + ")"
-		      + " scale(" + d3.event.scale + ")");
-		}
+	  	 .append('svg:g')
+ 	    	.call(d3.behavior.zoom().on("zoom", redraw))
+	  	 .append('svg:g')
+		 
 	
-	vis.append('svg:rect')
-	    .attr('width', width)
-	    .attr('height', height)
-	    .attr('fill', 'white');
-		
-	var loading = vis.append("text")
-	    .attr("x", width / 2)
-	    .attr("y", height / 2)
-	    .attr("dy", ".35em")
-	    .style("text-anchor", "middle")
-	    .text("Simulating Layout. One moment please…");
+ 	var rect = vis.append('svg:rect')
+ 	    .attr('width', width)
+ 	    .attr('height', height)
+ 	    .attr('fill', 'white');	
 	
+	function redraw() {
+        $(".pop-up").fadeOut(50);
+		previousd="";
+		trans=d3.event.translate;
+		scale=d3.event.scale;
+
+		vis.attr("transform","translate(" + trans + ")"+" scale(" + scale + ")");
+	}
 		
-	// draw
 	d3.json(NETWORK_LOCAL_DATA_URI, function(error, graph) {
-	  force
-	      .nodes(graph.nodes)
-	      .links(graph.links)
-	      .start();	
-    
+		var force = d3.layout.force()
+	        // .charge(-120)
+	        // .linkDistance(30)
+			.links(graph.links)
+			.nodes(graph.nodes)
+		    .size([width, height]);
+	  	      	//.start();	
+	    
 		var link = vis.selectAll("line.link")
 	        .data(graph.links)
 	        .enter().append("svg:line")
 	      	.style("stroke-width", function(d) { return d.accumulated+1; })
-	        .attr("class", "link");
+	        .attr("class", "link")
+			.on("click",lover);
 		var node_drag = d3.behavior.drag()
 		    .on("dragstart", dragstart)
 		    .on("drag", dragmove)
 		    .on("dragend", dragend);
+			
 		function dragstart(d, i) {
 		    force.stop() // stops the force auto positioning before you start dragging
-			console.log("inside")
+	        $(".pop-up").fadeOut(50);
 		}
-
+	
 		function dragmove(d, i) {
 		    d.px += d3.event.dx;
 		    d.py += d3.event.dy;
@@ -82,20 +84,25 @@
 		    d.y += d3.event.dy; 
 		    tick(); // this is the key to make it work together with updating both px,py,x,y on d !
 		}
-
+	
 		function dragend(d, i) {
 		    d.fixed = true; // of course set the node to fixed so the force doesn't include the node in its auto positioning stuff
 		    tick();
 		}
+		
 		var gnodes = vis.selectAll('.node')
 			.data(graph.nodes)
 			.enter()
 			.append('g')
 			.call(node_drag)
+			.on("click",mover)
 			.classed('gnode', true);
 		var node = gnodes.append("circle")
 			.attr("class", "node")
-			.attr("r", 4.5)
+			.attr("r", r - .5)
+			// .on ("mouseout",mout)
+	        .style("stroke", function(d) { return d3.rgb(color(d.GO_ref)).darker(); })
+	        .style("stroke-width", 0.5)
 			.style("fill", function(d) { return color(d.GO_ref); });
 		var labels = gnodes.append("text")
 	    	.attr("dy", ".3em")
@@ -114,13 +121,77 @@
 	  		    .attr("y1", function(d) { return d.source.y; })
 	  		    .attr("x2", function(d) { return d.target.x; })
 	  		    .attr("y2", function(d) { return d.target.y; });
-
+								
 	  		  // Translate the groups
 	  		  gnodes.attr("transform", function(d) { 
 	  		    return 'translate(' + [d.x, d.y] + ')'; 
-			});
+				});				
 		}
+	    function mover(d,i) {
+	        $(".pop-up").fadeOut(50);
+			if(d.name != previousd){
+				previousd = d.name;
+		        $("#pop-up-node").fadeOut(100,function () {
+		            // Popup content
+		            $("#node-title").html("Protein: " + d.main + " (" + d.name + ")");
+		            $("#nodename").html("Name: "+ d.Protein_names);
+		            $("#uniprot").html("Uniprot: "+ d.Entry);
+		            $("#gofun").html("Enriched Function: "+ d.GO_ref);
+		            $("#gofundesc").html("GO description: " + d.GOref_description);
+					var enrichpval;
+					if(d.reference_pval == "NA"){
+						enrichpval="NA"
+					}else{
+						enrichpval=parseFloat(d.reference_pval.toPrecision(5));
+					}
+					$("#gofunpval").html("Enrichment P-value: "+ enrichpval) ;
+					
+		            // $("#pop-desc").html("M+T: text text test");
+
+		            // Popup position
+		            var popLeft = (d.x*scale)+trans[0]+20;//lE.cL[0] + 20;
+		            var popTop = (d.y*scale)+trans[1]+20;//lE.cL[1] + 70;
+		            $("#pop-up-node").css({"left":popLeft,"top":popTop});
+		            $("#pop-up-node").fadeIn(100);
+		        });
+			}else{
+				previousd = "";
+			}
+	    }
 		
+		
+	    function lover(d,i) {
+	        $(".pop-up").fadeOut(50);
+			if(d.name != previousd){
+				previousd = d.name;
+		        $("#pop-up-link").fadeOut(100,function () {
+		            // Popup content
+		            $("#link-title").html("Interaction: " + d.name);
+					$("#orthologs").html("Orthologs in common: "+ d.n);
+					$("#mt").html("Mirrortree: "+ Math.round(d.r).toFixed(5));			
+					var pval;
+					if(d.p_value == 0){
+						pval="< 1E-3";
+					}else{
+						pval=d.p_value;
+					}
+					$("#pMT").html("p-mirrortree P-value: "+ pval);
+					$("#context").html("ContextMirror (level 10): "+ d.context_lvl10);
+					
+		            // Popup position
+		            var popLeft = (((d.source.x + d.target.x)/2)*scale)+trans[0]+20;//lE.cL[0] + 20;
+		            var popTop = (((d.source.y + d.target.y)/2)*scale)+trans[1]+20;//lE.cL[1] + 70;
+		            $("#pop-up-link").css({"left":popLeft,"top":popTop});
+		            $("#pop-up-link").fadeIn(100);
+		        });
+			}else{
+				previousd = "";
+			}
+	    }
+		
+	    // function mout(d) {
+	    //     //d3.select(this).attr("fill","url(#ten1)");
+	    // }
 	  	setTimeout(function() {
 	  		  // Run the layout a fixed number of times.
 	  		  // The ideal number of times scales with graph complexity.
@@ -141,8 +212,10 @@
 	  		  //   .enter().append("circle")
 	  		  //     .attr("cx", function(d) { return d.x; })
 	  		  //     .attr("cy", function(d) { return d.y; })
-	  		  loading.remove();
-	  		}, 10);		  
+			  $("#loadingCon").fadeOut();
+	  		}, 10);	
+			console.log( document.getElementsByTagName('*').length )	  
 	});
+	
 }).call(this);
 
